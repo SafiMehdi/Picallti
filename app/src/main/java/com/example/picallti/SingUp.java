dc3a5d8b521432fa646fc369a161a9c5e7a38fcd
hervelinne <mimissa20@hotmail.fr>
2022-11-12 23:02:52 +0100

    adding the Sign Up page

---
+++ app/src/main/java/com/example/picallti/SingUp.java
@@ -0,0 +1,26 @@
+package com.example.picallti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class SingUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+
+        Spinner spinner = (Spinner) findViewById(R.id.cities_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.cities_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+    }
+}

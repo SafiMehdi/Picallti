8b27de74b54c3bfa02e5a3f95b89347b4f7aa93e
Ibtissam Zerouali <ibtissamzerouali7@gmail.com>
2022-11-13 12:49:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/picallti/ContactUsActivity.java
@@ -0,0 +1,56 @@
+package com.example.picallti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @BindView(R.id.ContactName)
+    EditText contactName;
+    @BindView(R.id.ContactMail)
+    EditText contactMail;
+    @BindView(R.id.ContactMsg)
+    EditText contactMsg;
+    @BindView(R.id.ContactSendBtn)
+    Button contactSendBtn;
+    @BindView(R.id.ContactBack)
+    ImageButton contactBack ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+        ButterKnife.bind(this);
+
+        contactSendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //here we are going to inplement intent that containt alrealdy the destination mail and recuperate the infon from the
+                //editTexts thes send email dairectly
+                    Toast.makeText(getApplicationContext(), "Send succesfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+            }
+        });
+
+        contactBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+
+
+    }
+}

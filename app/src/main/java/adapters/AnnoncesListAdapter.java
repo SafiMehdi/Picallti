490138ac50248b8a985d740facc445739001f192
SafiMehdi <safii.elmehdi@gmail.com>
2022-11-15 01:19:08 +0100

    Add activity for announcements(unfinished)

---
+++ app/src/main/java/adapters/AnnoncesListAdapter.java
@@ -0,0 +1,53 @@
+package com.example.picallti;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import org.w3c.dom.Text;
+
+public class AnnoncesListAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] maintitle;
+    private final String[] description;
+    private final String[] localisation;
+    private final String[] prix;
+    private final Integer[] imgid;
+
+    public MyListAdapter(Activity context, String[] maintitle,String[] description,String[] localisation, String[] prix, Integer[] imgid) {
+        super(context, R.layout.activity_listcomp, maintitle);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.maintitle=maintitle;
+        this.description=description;
+        this.localisation=localisation;
+        this.prix=prix;
+        this.imgid=imgid;
+
+    }
+
+    public View getView(int position,View view,ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.activity_annonces_list, null,true);
+
+        TextView titleText = (TextView) rowView.findViewById(R.id.titreAnnonces);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        TextView desc = (TextView) rowView.findViewById(R.id.descriptionAnnonces);
+        TextView price = (TextView) rowView.findViewById(R.id.prixAnnonces);
+        TextView location = (TextView) rowView.findViewById(R.id.localisationAnnonces);
+
+        titleText.setText(maintitle[position]);
+        imageView.setImageResource(imgid[position]);
+        desc.setText(description[position]);
+        price.setText(prix[position]);
+        location.setText(localisation[position]);
+
+        return rowView;
+
+    };
+}
--- app/src/main/java/com/example/picallti/MainActivity.java
+++ app/src/main/java/com/example/picallti/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.picallti;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    ListView list;
+
+    String[] maintitle ={
+            "Velo BMX","Velo VTT",
+    };
+
+    String[] description ={
+            "sqldkqsd qlskd qlsd qsd qslkdjqslk jdqsdia mlda q","sqldkqsd qlskd qlsd qsd qslkdjqslk jdqsdia mlda q",
+    };
+
+    String[] localisation ={
+            "Rabat, Maroc", "Casablanca, Maroc","Kenitra, Maroc"
+    };
+
+    String[] prix ={
+            "12DH/Jours", "10DH/Jours",
+    };
+
+    Integer[] imgid={
+            R.drawable.profile_circle,R.drawable.profile_circle,
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
+
+        MyListAdapter adapter=new MyListAdapter(this, maintitle, description, localisation, prix, imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
     }
 }

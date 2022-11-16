9ec8795bc1a71911171f269716e8d7a0da2409f7
fad232 <fadixfoxer@gmail.com>
2022-11-16 20:35:51 +0100

    third commit

--- app/src/main/java/com/example/picallti/login_page.java
+++ app/src/main/java/com/example/picallti/login_page.java
@@ -6,15 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class login_page extends AppCompatActivity {
-    Button btn = (Button)findViewById(R.id.btnConnect);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-        btn.setOnClickListener(new View.OnClickListener() {
+        TextView txt = (TextView)findViewById(R.id.forgotbtn);
+        txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(login_page.this, forgot_pass1.class));

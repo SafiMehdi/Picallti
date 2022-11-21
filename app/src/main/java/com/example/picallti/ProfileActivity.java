a7ce314406eeb12c35ecb2b954554dc01f6e6cf4
SafiMehdi <safii.elmehdi@gmail.com>
2022-11-21 02:02:07 +0100

    Fix ID duplication in certain activities

--- app/src/main/java/com/example/picallti/ProfileActivity.java
+++ app/src/main/java/com/example/picallti/ProfileActivity.java
@@ -9,35 +9,7 @@ import android.widget.Button;
 import android.widget.ListView;
 
 public class ProfileActivity extends AppCompatActivity {
-    ListView list;
-    Button modifierProfileButton;
     BottomBarFragment frag = new BottomBarFragment();
-
-    String[] maintitle ={
-            "Velo BMX","Velo VTT","Chi pikala zwina"
-    };
-
-    String[] description ={
-            "sqldkqsd qlskd qlsd qsd qslkdjqslk jdqsdia mlda q","sqldkqsd qlskd qlsd qsd qslkdjqslk jdqsdia mlda q","sqldkqsd qlskd qlsd qsd qslkdjqslk jdqsdia mlda q"
-    };
-
-    String[] localisation ={
-            "Rabat, Maroc", "Casablanca, Maroc","Kenitra, Maroc"
-    };
-
-    String[] prix ={
-            "12DH/Jours", "10DH/Jours","8DH/Jours"
-    };
-
-    String[] temps ={
-            "17 minutes", "2 Heures","2 Jours"
-    };
-
-    Integer[] imgid={
-            R.drawable.profile_circle,R.drawable.profile_circle,R.drawable.profile_circle
-    };
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

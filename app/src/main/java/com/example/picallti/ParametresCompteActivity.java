b072d7700a65a6bafa040250054e62ba7f3417b6
fad232 <fadixfoxer@gmail.com>
2022-11-25 02:40:31 +0100

    ajouter annonce is done but need some improving

--- app/src/main/java/com/example/picallti/ParametresCompteActivity.java
+++ app/src/main/java/com/example/picallti/ParametresCompteActivity.java
@@ -17,30 +17,6 @@ public class ParametresCompteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres_compte);
 
-        TextView deleteAccount = (TextView)findViewById(R.id.deleteAccount);
-        deleteAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ParametresCompte.this, DeleteAccount.class));
-            }
-        });
-
-        TextView forgottenmdp = (TextView)findViewById(R.id.mdpForgotten);
-        forgottenmdp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ParametresCompte.this, forgot_pass2.class));
-            }
-        });
-
-        TextView disconnect = (TextView)findViewById(R.id.disconnect);
-        disconnect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText( ParametresCompte.this, "You have been disconnected!",Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(ParametresCompte.this, login_page.class));
-            }
-        });
 
 
     }

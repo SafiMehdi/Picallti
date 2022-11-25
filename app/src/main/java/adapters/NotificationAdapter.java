c78350ee049471d7119f9580a571fb574e71c12d
SafiMehdi <safii.elmehdi@gmail.com>
2022-11-25 15:16:09 +0100

    Change notifications activity ui

--- app/src/main/java/adapters/NotificationAdapter.java
+++ app/src/main/java/adapters/NotificationAdapter.java
@@ -1,53 +1,62 @@
 package adapters;
 
-import android.content.Context;
-import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import com.example.picallti.R;
-
-import java.text.DateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
 import java.util.ArrayList;
 
 import data.Notification;
+import com.example.picallti.R;
 
-public class NotificationAdapter extends ArrayAdapter<Notification> {
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
 
     ArrayList<Notification> notifications;
 
-    public NotificationAdapter(@NonNull Context context, int resource, ArrayList<Notification> notifications) {
-        super(context, resource, notifications);
+    public NotificationAdapter(ArrayList<Notification> notifications) {
         this.notifications = notifications;
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(getContext().LAYOUT_INFLATER_SERVICE);
-        convertView = inflater.inflate(R.layout.notification_list_element,parent,false);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_holder_notification, parent, false);
+        return new ViewHolder(inflate);
+    }
 
-        ImageView imageView = convertView.findViewById(R.id.notification_icon);
-        imageView.setBackgroundResource(notifications.get(position).getIcon());
-        TextView title = convertView.findViewById(R.id.notification_title);
-        title.setText(notifications.get(position).getTitle());
-        if(notifications.get(position).getText()!= null){
-            TextView text = convertView.findViewById(R.id.notification_text);
-            text.setText(notifications.get(position).getText());
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.titleTxt.setText(String.valueOf(notifications.get(position).getTitle()));
+        holder.descriptionTxt.setText(String.valueOf(notifications.get(position).getText()));
+        holder.timeTxt.setText(String.valueOf(notifications.get(position).getTime()));
+
+        int drawableResourceId = holder.itemView.getContext().getResources().getIdentifier(notifications.get(position).getUrl(), "drawable", holder.itemView.getContext().getPackageName());
+
+        Glide.with(holder.itemView.getContext())
+                .load(drawableResourceId)
+                .into(holder.removeItem);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return notifications.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTxt,descriptionTxt,timeTxt;
+        ImageView removeItem;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            descriptionTxt = itemView.findViewById(R.id.descriptionTxt);
+            timeTxt = itemView.findViewById(R.id.timeTxt);
+            removeItem = itemView.findViewById(R.id.img_view);
         }
-        DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(getContext());
-        //mTimeText.setText("Time: " + dateFormat.format(date));
-        TextView time= convertView.findViewById(R.id.notification_time);
-        time.setText(dateFormat.format(notifications.get(position).getTime()).toString());
-        return convertView;
     }
 }

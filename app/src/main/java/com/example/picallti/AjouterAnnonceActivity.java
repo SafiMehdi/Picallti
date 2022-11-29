b8f4ceb0b4045021d28c1b4c9b204a063823e63b
IlhamOukassou <ilhmoukassou@gmail.com>
2022-11-29 16:56:51 +0100

    call owner

--- app/src/main/java/com/example/picallti/AjouterAnnonceActivity.java
+++ app/src/main/java/com/example/picallti/AjouterAnnonceActivity.java
@@ -2,11 +2,29 @@ package com.example.picallti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AjouterAnnonceActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.TitreOffre)
+    TextView titre;
+    @BindView(R.id.Description)
+    TextView description;
+    @BindView(R.id.Prix)
+    TextView prix;
+    @BindView(R.id.add)
+    ImageView addImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +46,32 @@ public class AjouterAnnonceActivity extends AppCompatActivity {
                 R.array.categorie, android.R.layout.simple_spinner_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void saveOffre(){
+        if(titre.getText().length() >100  ){
+            Toast.makeText(getApplicationContext(),R.string.titleTooLongMessage, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(titre.getText().length() == 0  ){
+            Toast.makeText(getApplicationContext(),R.string.noTitleMessage, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(prix.getText().length() == 0  ){
+            Toast.makeText(getApplicationContext(),R.string.noPriceMessage, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (Float.parseFloat(prix.getText().toString()) <=0){
+            Toast.makeText(getApplicationContext(),R.string.falsePriceMessage, Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    @OnClick(R.id.add)
+    public void addImage(){
+        startActivity(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI));
     }
 }
--- app/src/main/java/com/example/picallti/ModifierProfileActivity.java
+++ app/src/main/java/com/example/picallti/ModifierProfileActivity.java
@@ -15,14 +15,14 @@ public class ModifierProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modifier_profile);
 
-        Spinner spinner = (Spinner) findViewById(R.id.cities_spinner);
+        //Spinner spinner = (Spinner) findViewById(R.id.cities_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.cities_array, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        //spinner.setAdapter(adapter);
 
     }
 }

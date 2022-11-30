12dbd4fa871437fa3ec1f79f20dbe84d96366522
SafiMehdi <safii.elmehdi@gmail.com>
2022-11-30 13:55:47 +0100

    Add features on settings activity

--- app/src/main/java/com/example/picallti/ParametresActivity.java
+++ app/src/main/java/com/example/picallti/ParametresActivity.java
@@ -3,15 +3,14 @@ package com.example.picallti;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ParametresCompteActivity extends AppCompatActivity {
+public class ParametresActivity extends AppCompatActivity {
 
     BottomBarFragment frag = new BottomBarFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_parametres_compte);
-        getSupportFragmentManager().beginTransaction().add(R.id.bottom_bar_container,frag).commit();
+        setContentView(R.layout.activity_parametres);
 
     }
 }

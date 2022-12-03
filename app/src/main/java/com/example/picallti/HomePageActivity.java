e016e8c00a38ab5e38338f496cebfba76652047c
fad232 <fadixfoxer@gmail.com>
2022-12-03 20:03:53 +0100

    just few changes

--- app/src/main/java/com/example/picallti/HomePageActivity.java
+++ app/src/main/java/com/example/picallti/HomePageActivity.java
@@ -1,9 +1,13 @@
 package com.example.picallti;
 
+import static com.example.picallti.login_page.PREFS_NAME;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
@@ -12,22 +16,29 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class HomePageActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2500;
 
-    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(HomePageActivity.this, OffrePageActivity.class);
-                startActivity(intent);
-                finish();
+                SharedPreferences myPrefs = getSharedPreferences(PREFS_NAME, 0);
+                boolean hasLoggedIn = myPrefs.getBoolean("isCheckedPref", false);
+                if (hasLoggedIn) {
+                    Intent intent = new Intent(HomePageActivity.this, OffrePageActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(HomePageActivity.this, login_page.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
-        }, 5000);
+        }, SPLASH_TIME_OUT);
     }
 }

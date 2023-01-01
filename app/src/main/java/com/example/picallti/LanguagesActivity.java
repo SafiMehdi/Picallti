1d7878c5f850251ef2b88e2a1ed70acdbe9996e8
ouissal-elkorri <ouissal.elkorri@gmail.com>
2023-01-01 12:53:41 +0100

    languages page design with sidebar

--- app/src/main/java/com/example/picallti/LanguagesActivity.java
+++ app/src/main/java/com/example/picallti/LanguagesActivity.java
@@ -1,14 +1,80 @@
 package com.example.picallti;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
 
 public class LanguagesActivity extends AppCompatActivity {
 
+    //The function that implements the sidebar
+    public void Sidebar(){
+        NavigationView navView = findViewById(R.id.sidebar_view);
+        navView.inflateMenu(R.menu.sidebar_menu);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_profile:
+                        Intent intent_profile = new Intent(LanguagesActivity.this, ProfileActivity.class);
+                        startActivity(intent_profile);
+                        break;
+                    case R.id.nav_likes:
+                        Intent intent_likes = new Intent(LanguagesActivity.this, FavorisActivity.class);
+                        startActivity(intent_likes);
+                        break;
+                    case R.id.nav_langues:
+                        Intent intent_langues = new Intent(LanguagesActivity.this, LanguagesActivity.class);
+                        startActivity(intent_langues);
+                        break;
+                    case R.id.nav_apropos:
+                        Intent intent_apropos = new Intent(LanguagesActivity.this, AproposActivity.class);
+                        startActivity(intent_apropos);
+                        break;
+                    case R.id.nav_parametre:
+                        Intent intent_parametre = new Intent(LanguagesActivity.this, ParametresActivity.class);
+                        startActivity(intent_parametre);
+                        break;
+                    case R.id.nav_logout:
+                        Toast.makeText( LanguagesActivity.this, "You've been disconnected!",Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LanguagesActivity.this, login_page.class));
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
+            }
+        });
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout_languages_page);
+
+        ImageButton toggleButton = findViewById(R.id.sidebar_button);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Open or close the navigation drawer when the button is clicked
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_languages);
+        //Sidebar implementation
+        Sidebar();
     }
 }

71ba71fdd94bfce314b94f1c8a93697b57a2ceaa
nisrinemofakir <nisrinemofakir@gmail.com>
2023-01-08 03:02:28 +0100

    Link offersByUser with backend and display offers by user

--- app/src/main/java/retrofit/OffreApi.java
+++ app/src/main/java/retrofit/OffreApi.java
@@ -7,10 +7,14 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface OffreApi {
     @GET("/offers/getAll")
     Call<List<Offre>> getOffers();
+
+    @GET("/offers/getAllByUser")
+    Call<List<Offre>> getOffersByUser(@Query("id") int id);
 
     @POST("/offers/add")
     Call<Void> addOffre(@Body Offre offre);
